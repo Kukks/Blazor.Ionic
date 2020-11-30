06616540347d63b69443e3b0f944529a92833a4c
Kukks <evilkukka@gmail.com>
2020-11-30 11:37:00 +0100

    Refacotr input controls, add ionitem to handle href, incldue livesharp in deve nv

---
+++ Blazor.Ionic/BaseIonicInputComponent.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Blazor.Ionic
+{
+    public abstract class BaseIonicInputComponent<TInputType, TChangeEventDetail> : ComponentBase
+        where TChangeEventDetail : BaseIonicChangeEventDetail<TInputType>
+    {
+        private TInputType _value;
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<string, object> InputAttributes { get; set; }
+
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public TInputType Value
+        {
+            get => _value;
+            set
+            {
+                if (Equals(_value, value)) return;
+                _value = value;
+                ValueChanged.InvokeAsync(value);
+            }
+        }
+
+        [Parameter] public EventCallback<TInputType> ValueChanged { get; set; }
+
+        [Inject] protected IJSRuntime JsRuntime { get; set; }
+        protected DotNetObjectReference<BaseIonicInputComponent<TInputType, TChangeEventDetail>> ThisRef { get; set; }
+        protected  ElementReference Element { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                ThisRef = DotNetObjectReference.Create(this);
+                await JsRuntime.InvokeVoidAsync("IonicBridge.registerBlazorCustomHandler", Element, "ionChange",
+                    ThisRef, nameof(HandleChange));
+            }
+
+            await base.OnAfterRenderAsync(firstRender);
+        }
+
+        [JSInvokable(nameof(HandleChange))]
+        public virtual Task HandleChange(TChangeEventDetail detail)
+        {
+            Value = detail.GetValue();
+            return Task.CompletedTask;
+        }
+    }
+
+    public abstract class BaseIonicChangeEventDetail<TInputType>
+    {
+        public abstract TInputType GetValue();
+    }
+}
