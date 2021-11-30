c0b5b3b9a477015aecbe699fa99db799399fed62
Kukks <evilkukka@gmail.com>
2021-11-30 13:23:46 +0100

    fix some of the things for all of the things

--- Blazor.Ionic/BaseIonicValidationComponent.cs
+++ Blazor.Ionic/BaseIonicValidationComponent.cs
@@ -5,73 +5,75 @@ using System.Linq.Expressions;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 
-namespace Blazor.Ionic;
-
-public class BaseIonicValidationComponent : BaseIonicComponent, IBaseIonicValidationComponent
+namespace Blazor.Ionic
 {
-    private EditContext? _editContext;
 
-    protected Action<EditContext?>? EditContextChanging;
-    private FieldIdentifier? _validationFieldIdentifier;
-    private Expression<Func<object>>? _validationField;
-    private EditContext? _cascadingEditContext;
-
-    [Parameter]
-    public EditContext? EditContext
+    public class BaseIonicValidationComponent : BaseIonicComponent, IBaseIonicValidationComponent
     {
-        get => _editContext ?? CascadingEditContext;
-        set
+        private EditContext? _editContext;
+
+        protected Action<EditContext?>? EditContextChanging;
+        private FieldIdentifier? _validationFieldIdentifier;
+        private Expression<Func<object>>? _validationField;
+        private EditContext? _cascadingEditContext;
+
+        [Parameter]
+        public EditContext? EditContext
         {
-            EditContextChanging?.Invoke(value);
-            _editContext = value;
-        }
-    }
-
-    [CascadingParameter]
-    public EditContext? CascadingEditContext
-    {
-        get => _cascadingEditContext;
-        set
-        {
-            EditContextChanging?.Invoke(value);
-            _cascadingEditContext = value;
-        }
-    }
-
-    [Parameter]
-    public FieldIdentifier? ValidationFieldIdentifier
-    {
-        get => _validationFieldIdentifier ?? CascadingValidationFieldIdentifier ??
-            (ValidationField is null ? null : FieldIdentifier.Create(ValidationField));
-        set => _validationFieldIdentifier = value;
-    }
-
-    [CascadingParameter] public FieldIdentifier? CascadingValidationFieldIdentifier { get; set; }
-
-    [Parameter]
-    public Expression<Func<object>>? ValidationField
-    {
-        get => _validationField ?? CascadingValidationField;
-        set => _validationField = value;
-    }
-
-    [CascadingParameter] public Expression<Func<object>>? CascadingValidationField { get; set; }
-
-    public void NotifyFieldChanged()
-    {
-        if (EditContext != null && ValidationFieldIdentifier != null)
-        {
-            EditContext.NotifyFieldChanged(ValidationFieldIdentifier.Value);
-        }
-    }
-
-    public string[]? GetValidationMessages()
-    {
-        if (EditContext != null && ValidationFieldIdentifier != null)
-        {
-            return EditContext.GetValidationMessages(ValidationFieldIdentifier.Value)?.ToArray();
+            get => _editContext ?? CascadingEditContext;
+            set
+            {
+                EditContextChanging?.Invoke(value);
+                _editContext = value;
+            }
         }
 
-        return null;
+        [CascadingParameter]
+        public EditContext? CascadingEditContext
+        {
+            get => _cascadingEditContext;
+            set
+            {
+                EditContextChanging?.Invoke(value);
+                _cascadingEditContext = value;
+            }
+        }
+
+        [Parameter]
+        public FieldIdentifier? ValidationFieldIdentifier
+        {
+            get => _validationFieldIdentifier ?? CascadingValidationFieldIdentifier ??
+                (ValidationField is null ? null : FieldIdentifier.Create(ValidationField));
+            set => _validationFieldIdentifier = value;
+        }
+
+        [CascadingParameter] public FieldIdentifier? CascadingValidationFieldIdentifier { get; set; }
+
+        [Parameter]
+        public Expression<Func<object>>? ValidationField
+        {
+            get => _validationField ?? CascadingValidationField;
+            set => _validationField = value;
+        }
+
+        [CascadingParameter] public Expression<Func<object>>? CascadingValidationField { get; set; }
+
+        public void NotifyFieldChanged()
+        {
+            if (EditContext != null && ValidationFieldIdentifier != null)
+            {
+                EditContext.NotifyFieldChanged(ValidationFieldIdentifier.Value);
+            }
+        }
+
+        public string[]? GetValidationMessages()
+        {
+            if (EditContext != null && ValidationFieldIdentifier != null)
+            {
+                return EditContext.GetValidationMessages(ValidationFieldIdentifier.Value)?.ToArray();
+            }
+
+            return null;
+        }
     }
 }
--- Blazor.Ionic/IBaseIonicValidationComponent.cs
+++ Blazor.Ionic/IBaseIonicValidationComponent.cs
@@ -3,16 +3,17 @@ using System;
 using System.Linq.Expressions;
 using Microsoft.AspNetCore.Components.Forms;
 
-namespace Blazor.Ionic;
-
-public interface IBaseIonicValidationComponent
+namespace Blazor.Ionic
 {
-    EditContext? EditContext { get; set; }
-    EditContext? CascadingEditContext { get; set; }
-    FieldIdentifier? ValidationFieldIdentifier { get; set; }
-    FieldIdentifier? CascadingValidationFieldIdentifier { get; set; }
-    Expression<Func<object>>? ValidationField { get; set; }
-    Expression<Func<object>>? CascadingValidationField { get; set; }
-    void NotifyFieldChanged();
-    string[]? GetValidationMessages();
+    public interface IBaseIonicValidationComponent
+    {
+        EditContext? EditContext { get; set; }
+        EditContext? CascadingEditContext { get; set; }
+        FieldIdentifier? ValidationFieldIdentifier { get; set; }
+        FieldIdentifier? CascadingValidationFieldIdentifier { get; set; }
+        Expression<Func<object>>? ValidationField { get; set; }
+        Expression<Func<object>>? CascadingValidationField { get; set; }
+        void NotifyFieldChanged();
+        string[]? GetValidationMessages();
+    }
 }
